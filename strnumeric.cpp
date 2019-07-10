0d1c4ed117d91c4ae47e726d8fbcbf6f8da83fbf
pravinkumarkg <35860045+pravinkumarkg@users.noreply.github.com>
2019-07-10 16:44:37 +0530

    Create strnumeric.cpp

---
+++ strnumeric.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char str;
+	cin>>str;
+	if(str=='1'||str=='2'||str=='3'||str=='4'||str=='5'||str=='6'||str=='7'||str=='8'||str=='9'||str=='0')
+	{
+		cout<<"yes";
+	}
+	else
+	{
+		cout<<"No";
+	}	
+	return 0;
+}
