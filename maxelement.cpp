7dbf5847813fe531cbb77a304be0874a83fb330b
pravinkumarkg <35860045+pravinkumarkg@users.noreply.github.com>
2019-07-10 15:23:50 +0530

    Create maxelement.cpp

---
+++ maxelement.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,n,arr[50],largest;
+	cin>>n;
+	for(i=0;i<n;i++)
+	
+		cin>>arr[i];
+		
+	largest =arr[0];
+	for(i=1;i<n;i++)
+	{
+		if(largest<arr[i])
+		
+			largest=arr[i];
+		
+			
+	}
+	cout<<largest;
+
+	
+	return 0;
+}
