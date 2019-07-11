e7df1088d14fb8340c8534f43ec09fe6d6dad3a4
pravinkumarkg <35860045+pravinkumarkg@users.noreply.github.com>
2019-07-11 16:38:38 +0530

    Create subtime.cpp

---
+++ subtime.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int hour1, min1, hour2, min2;
+    int resHour, resMin;
+    
+    cin >> hour1 >> min1 >> hour2 >> min2;
+    if( hour1>hour2 && min2 > min1)
+    {
+        hour1--;
+        min1 += 60;
+    }
+    else if(hour2 > hour1 && min1 > min2)
+    {
+    	hour2--;
+    	min2 += 60;
+    }
+    resHour = hour1 - hour2;
+    resMin = min1 - min2;
+    cout << (resHour < 0 ? -resHour : resHour) << " " << (resMin? -resMin : resMin);
+    
+    return 0;
+    
+}
