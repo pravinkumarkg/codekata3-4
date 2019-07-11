9eaca2e25fbcfeb43ddfe7e30a2041fb7bbfb419
pravinkumarkg <35860045+pravinkumarkg@users.noreply.github.com>
2019-07-11 17:04:23 +0530

    Create linecount.cpp

---
+++ linecount.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char str[50];
+	int count=0;
+	cin.getline(str, 50);
+	for(int i=0;str[i]!='\0';i++)
+	{
+	
+		if(str[i]=='.')
+		count++;
+	}
+	cout<<count;
+	return 0;
+}
