561235429e689b0f83963d5158a4d72792c7612a
pravinkumarkg <35860045+pravinkumarkg@users.noreply.github.com>
2019-07-11 16:12:12 +0530

    Update charcount.cpp

--- charcount.cpp
+++ charcount.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int main() {
 	char str[20];
 	int count=0;
-	cin>>str;
-	for(int i=0;i<str[i];i++)
+	cin.getline(str, 10);
+	for(int i=0;str[i]!='\0';i++)
 	{
 		count++;
+		if(	str[i]==' ')
+		count--;
 	}
 	cout<<count;
 	return 0;
