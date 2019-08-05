20572ae585b4c52f9ec1accc0db73749258ebed2
pravinkumarkg <35860045+pravinkumarkg@users.noreply.github.com>
2019-08-05 21:50:48 +0530

    Create fib.c

---
+++ fib.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ void main()
+{
+    int fib1 = 1, fib2 = 1, fib3, num, count = 0;
+	scanf("%d", &num);
+	printf("%d\t%d\t", fib1,fib2);
+    count = 2; 
+    while (count < num)
+    {
+        fib3 = fib1 + fib2;
+        count++;
+        printf("%d\n", fib3);
+        fib1 = fib2;
+        fib2 = fib3;
+   }
+}
